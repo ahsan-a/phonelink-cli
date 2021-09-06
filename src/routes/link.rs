81a62e319cf13eebf7fe1686bdc30ba7771c30a0
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-09-06 17:20:17 +0100

    removed opener function since it doens't need to be used three times

--- src/routes/link.rs
+++ src/routes/link.rs
@@ -7,10 +7,6 @@ pub async fn link_route(web::Path(mut url): web::Path<String>) -> impl Responder
         url = format!("https://{}", url)
     }
 
-    open_url(url)
-}
-
-fn open_url(url: String) -> impl Responder {
     match open(&url) {
         Ok(_) => format!("Successfully opened url {} on your computer.", url),
         Err(_) => format!("Failed to open url {} on your computer.", url),

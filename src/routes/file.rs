a70d093525644cf7e901b486c88c07c27a981224
Ahsan Ahmed <50714675+ahsan-a@users.noreply.github.com>
2024-04-08 14:53:12 +0100

    updated dependencies

--- src/routes/file.rs
+++ src/routes/file.rs
@@ -8,59 +8,59 @@ use std::path::Path;
 
 #[post("/file")]
 pub async fn file_route(
-    mut payload: Multipart,
-    data: web::Data<AppState>,
-    req: HttpRequest,
+	mut payload: Multipart,
+	data: web::Data<AppState>,
+	req: HttpRequest,
 ) -> String {
-    if !check_password(&req, &data).await {
-        return "Incorrect password.".to_string();
-    }
+	if !check_password(&req, &data).await {
+		return "Incorrect password.".to_string();
+	}
 
-    // iterate over multipart stream
-    let config = &data.config;
-    let dir_path = match config.get("save_path") {
-        Some(x) => x,
-        None => return "Failed: invalid save path".to_string(),
-    };
+	// iterate over multipart stream
+	let config = &data.config;
+	let dir_path = match config.get("save_path") {
+		Some(x) => x,
+		None => return "Failed: invalid save path".to_string(),
+	};
 
-    while let Ok(Some(mut field)) = payload.try_next().await {
-        let content_type = field.content_disposition().unwrap();
+	while let Ok(Some(mut field)) = payload.try_next().await {
+		let content_type = field.content_disposition().unwrap();
 
-        let filename = content_type.get_filename().unwrap();
-        let filext = Path::new(filename)
-            .extension()
-            .and_then(OsStr::to_str)
-            .unwrap();
+		let filename = content_type.get_filename().unwrap();
+		let filext = Path::new(filename)
+			.extension()
+			.and_then(OsStr::to_str)
+			.unwrap();
 
-        let mut filepath = format!("{}{}", dir_path, sanitize_filename::sanitize(&filename));
+		let mut filepath = format!("{}{}", dir_path, sanitize_filename::sanitize(&filename));
 
-        if std::path::Path::new(&filepath).exists() {
-            let mut i = 0;
+		if std::path::Path::new(&filepath).exists() {
+			let mut i = 0;
 
-            while std::path::Path::new(&filepath).exists() {
-                i += 1;
-                filepath = format!(
-                    "{}{} ({}).{}",
-                    dir_path,
-                    &filename.replace(&format!(".{}", filext), ""),
-                    i,
-                    filext
-                );
-            }
-        }
+			while std::path::Path::new(&filepath).exists() {
+				i += 1;
+				filepath = format!(
+					"{}{} ({}).{}",
+					dir_path,
+					&filename.replace(&format!(".{}", filext), ""),
+					i,
+					filext
+				);
+			}
+		}
 
-        // what does this do?
-        let mut f = web::block(|| std::fs::File::create(filepath))
-            .await
-            .unwrap();
+		// what does this do?
+		let mut f = web::block(|| std::fs::File::create(filepath))
+			.await
+			.unwrap();
 
-        while let Some(chunk) = field.next().await {
-            let data = chunk.unwrap();
-            f = web::block(move || f.write_all(&data).map(|_| f))
-                .await
-                .unwrap();
-        }
-    }
+		while let Some(chunk) = field.next().await {
+			let data = chunk.unwrap();
+			f = web::block(move || f.write_all(&data).map(|_| f))
+				.await
+				.unwrap();
+		}
+	}
 
-    format!("Your file was saved successfully in {}.", dir_path)
+	format!("Your file was saved successfully in {}.", dir_path)
 }
